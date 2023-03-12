460a0ddc919f4e697340df699098a58597e63059
unknown <a.ptashysnkyi@gmail.com>
2023-03-12 16:41:19 +0100

    Add chat section

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Message.scss";
 
 export const Message = () => {
+  const message =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis   similique maiores corrupti consectetur inventore facere deleniti   perferendis dolorem ex harum.";
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">MESSAGES</Link>> John Doe
+        </span>
+        <hr />
+        <div className="messages">
+          <div className="item">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item owner">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item owner">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item owner">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item owner">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+          <div className="item owner">
+            <img src="/imgs/avatar.jpg" alt="" />
+            <p>{message}</p>
+          </div>
+        </div>
+        <hr />
+        <div className="write">
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Write a message"
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
