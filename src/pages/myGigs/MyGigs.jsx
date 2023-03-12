9d12680d45596d0a52635508db07c2992130185a
unknown <a.ptashysnkyi@gmail.com>
2023-03-12 14:23:17 +0100

    Add Gigs page :wq

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./MyGigs.scss";
 
 export const MyGigs = () => {
   return (
-    <div>MyGigs</div>
-  )
-}
+    <div className="myGigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add" className="addBtn">
+            Add new gig
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/delete.svg" alt="" className="delete" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/delete.svg" alt="" className="delete" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/delete.svg" alt="" className="delete" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/delete.svg" alt="" className="delete" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
