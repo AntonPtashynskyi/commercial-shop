20bbdd688cf9708313fa5864c7f5381aae27bf52
unknown <a.ptashysnkyi@gmail.com>
2023-03-12 16:15:07 +0100

    add orders + messages section

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Messages.scss";
 
 export const Messages = () => {
+  const message =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque atque ea veniam ut at corporis natus nam aut incidunt minus?";
   return (
-    <div>Messages</div>
-  )
-}
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>Thomas Brush</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)} ...
+              </Link>
+            </td>
+            <td>1h ago</td>
+            <td>
+              <button className="readMsgBtn">Mask as read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>Thomas Brush</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)} ...
+              </Link>
+            </td>
+            <td>2h ago</td>
+            <td>
+              <button className="readMsgBtn">Mask as read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Thomas Brush</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)} ...
+              </Link>
+            </td>
+            <td>1 week ago</td>
+          </tr>
+          <tr>
+            <td>Thomas Brush</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)} ...
+              </Link>
+            </td>
+            <td>1 month ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React from "react";
+import "./Order.scss";
 
 export const Orders = () => {
+  const currentUser = {
+    id: 1,
+    userName: "John Doe",
+    isSeller: true,
+  };
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            {currentUser?.isSeller ? <th>Buyer</th> : <th>Seller</th>}
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/envelope.svg" alt="" className="envelope" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/envelope.svg" alt="" className="envelope" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/envelope.svg" alt="" className="envelope" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src="/imgs/item.jpg" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>59.99 $</td>
+            <td>43</td>
+            <td>
+              <img src="/imgs/envelope.svg" alt="" className="envelope" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
