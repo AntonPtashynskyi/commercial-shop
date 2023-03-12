7b977f883f6f8375a20e1d676c32e3e4b17d59cf
unknown <a.ptashysnkyi@gmail.com>
2023-03-12 14:01:37 +0100

    add gig page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,7 +1,234 @@
-import React from 'react'
+import React from "react";
+import "./Gig.scss";
 
 export const Gig = () => {
   return (
-    <div>Gig</div>
-  )
-}
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <div className="bradCrumbs">Fiver > Graphics & design ></div>
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+          <div className="user">
+            <img src="/imgs/avatar.jpg" alt="avatar" />
+            <span className="name">Anna Bell</span>
+            <div className="stars">
+              <img src="/imgs/star.svg" alt="" />
+              <img src="/imgs/star.svg" alt="" />
+              <img src="/imgs/star.svg" alt="" />
+              <img src="/imgs/star.svg" alt="" />
+              <img src="/imgs/star.svg" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <div className="slider">SLIDER</div>
+          <h2 className="about-title">Lorem, ipsum dolor.</h2>
+          <p className="about-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+            quae. Quae perferendis rerum maiores, odit error quibusdam harum
+            atque iste quo nobis unde commodi nisi dicta autem soluta illo.
+            Quas!
+          </p>
+          <div className="seller">
+            <h2>About User</h2>
+            <div className="user">
+              <div className="box-info">
+                <img src="/imgs/avatar.jpg" alt="" className="avatar" />
+                <div className="info">
+                  <span>Name Name</span>
+                  <div className="stars">
+                    <img src="/imgs/star.svg" alt="" />
+                    <img src="/imgs/star.svg" alt="" />
+                    <img src="/imgs/star.svg" alt="" />
+                    <img src="/imgs/star.svg" alt="" />
+                    <img src="/imgs/star.svg" alt="" />
+                    <span>5</span>
+                  </div>
+                  <button>Contact me</button>
+                </div>
+              </div>
+              <div className="box">
+                <div className="items">
+                  <div className="item">
+                    <span className="title">From</span>
+                    <span className="description">Ukraine</span>
+                  </div>
+                  <div className="item">
+                    <span className="title">Member since</span>
+                    <span className="description">Aug 2022</span>
+                  </div>
+                  <div className="item">
+                    <span className="title">Avr. response time</span>
+                    <span className="description">2 hours</span>
+                  </div>
+                  <div className="item">
+                    <span className="title">Last delivery</span>
+                    <span className="description">1 dey ago</span>
+                  </div>
+                  <div className="item">
+                    <span className="title">Language</span>
+                    <span className="description">English</span>
+                  </div>
+                </div>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur neque reprehenderit distinctio id error quia
+                  consectetur cupiditate vel nobis perspiciatis!
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <img src="/imgs/avatar.jpg" alt="avatar" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="/imgs/usa-flag.png" alt="flag" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Architecto, odit unde. Commodi nisi eos nobis voluptatum beatae.
+                Rem fugit maxime vitae nihil aliquid enim, tenetur voluptatibus
+                repudiandae ad, accusamus nisi architecto reiciendis vel,
+                ratione molestiae? Tenetur quia deserunt odio magni.
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/imgs/like.svg" alt="like" />
+                <span>Yes</span>
+                <img src="/imgs/dislike.svg" alt="dislike" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img src="/imgs/avatar.jpg" alt="avatar" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="/imgs/usa-flag.png" alt="flag" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Architecto, odit unde. Commodi nisi eos nobis voluptatum beatae.
+                Rem fugit maxime vitae nihil aliquid enim, tenetur voluptatibus
+                repudiandae ad, accusamus nisi architecto reiciendis vel,
+                ratione molestiae? Tenetur quia deserunt odio magni.
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/imgs/like.svg" alt="like" />
+                <span>Yes</span>
+                <img src="/imgs/dislike.svg" alt="dislike" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img src="/imgs/avatar.jpg" alt="avatar" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="/imgs/usa-flag.png" alt="flag" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <img src="/imgs/star.svg" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Architecto, odit unde. Commodi nisi eos nobis voluptatum beatae.
+                Rem fugit maxime vitae nihil aliquid enim, tenetur voluptatibus
+                repudiandae ad, accusamus nisi architecto reiciendis vel,
+                ratione molestiae? Tenetur quia deserunt odio magni.
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/imgs/like.svg" alt="like" />
+                <span>Yes</span>
+                <img src="/imgs/dislike.svg" alt="dislike" />
+                <span>No</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="content">
+            <div className="title">
+              <p>Lorem, ipsum dolor.</p>
+              <spam className="price">$ 59.99</spam>
+            </div>
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Consequatur minima praesentium dolorum mollitia debitis,
+              perspiciatis laborum perferendis. Voluptates, nemo alias?
+            </p>
+            <div className="details">
+              <div className="time">
+                <img src="/imgs/time.svg" alt="" />
+                <span>Days Delivery</span>
+              </div>
+              <div className="revision">
+                <img src="/imgs/reload.svg" alt="" />
+                <span>3 Revisions</span>
+              </div>
+            </div>
+            <div className="features">
+              <div className="item">
+                <img src="/imgs/check-alt.svg" alt="" />
+                <span>Promt waiting</span>
+              </div>
+              <div className="item">
+                <img src="/imgs/check-alt.svg" alt="" />
+                <span>ArtWork delivery</span>
+              </div>
+              <div className="item">
+                <img src="/imgs/check-alt.svg" alt="" />
+                <span>Image Upscaling</span>
+              </div>
+              <div className="item">
+                <img src="/imgs/check-alt.svg" alt="" />
+                <span>Additional design</span>
+              </div>
+            </div>
+            <button>Continue</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
